285a2a39d3114b51404ecc490f6534f0239cc798
USER <USER@USER-PC28>
2019-09-06 17:59:31 +0900

    SONG
    1.
    
    add 'sign up' .jsp
    
    2.
    
    form change in 'sign up' .jsp
    
    	add input type button : 'password verification'
    
    	add input type text : 'phone'
    
    	change type text -> date on 'Date of Birth'
    
    3.
    
    add e-mail Duplicate button
    
    (add method in userController, userService)
    
    userController .java
    
    	e-mail check method
    
    	if(null) Add attribute in model : 'msg', 'success'
    
    	else Add attribute in model : 'msg', 'failure'
    
    
    못 하는 영어 쓰느라 죽겠다 ㅠㅠ
    다음 주 해야될 일
    'submit form' 클릭시 나이 계산 DB 업데이트 return 메인page

--- zShopping/src/shopping/controller/UserController.java
+++ zShopping/src/shopping/controller/UserController.java
@@ -20,14 +20,12 @@ public class UserController {
 	
 	@Autowired
 	UserService userService;
-	
 	//로그인 화면
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
 	}
 	
-
 	
 	//로그인 처리
 	@RequestMapping("loginCheck")
@@ -60,5 +58,22 @@ public class UserController {
 		return "/main";
 	}
 	
-
+	//회원가입 화면
+	@RequestMapping("signUp")
+	public String signUp() {
+		return "/signUp";
+	}
+	
+	//email 중복 확인
+	@RequestMapping("emailCheck")
+	public Model emailCheck(@ModelAttribute UserVO user, HttpSession session) {
+		boolean result = userService.emailCheck(user, session);
+		Model model = null;
+		if(result) {
+			model.addAttribute("msg", "success");
+		}else {
+			model.addAttribute("msg", "failure");
+		}
+		return model;
+	}
 }
